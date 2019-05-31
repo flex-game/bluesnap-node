ea1b2f2aac01f6274a91c46d02aa7b32ae19de6a
smrubin <sam@storyblocks.com>
2019-05-31 12:39:31 -0400

    Remove batch gateway

--- src/lib/transaction/TransactionGateway.ts
+++ src/lib/transaction/TransactionGateway.ts
@@ -2,7 +2,6 @@ import CardTransactionGateway from './card/CardTransactionGateway';
 import WalletTransactionGateway from './wallet/WalletTransactionGateway';
 import EcpAchTransactionGateway from './alt/ecpAch/EcpAchTransactionGateway';
 import SepaDdTransactionGateway from './alt/sepaDirectDebit/SepaDdTransactionGateway';
-import BatchTransactionGateway from './batch/BatchTransactionGateway';
 import PayPalTransactionGateway from './alt/paypal/PayPalTransactionGateway';
 import HttpClient from '../http/HttpClient';
 
@@ -12,7 +11,6 @@ export default class MarketplaceGateway {
     public wallet: WalletTransactionGateway;
     public ecp: EcpAchTransactionGateway;
     public sepadd: SepaDdTransactionGateway;
-    public batch: BatchTransactionGateway;
     public paypal: PayPalTransactionGateway;
 
     public constructor(http: HttpClient) {
@@ -21,7 +19,6 @@ export default class MarketplaceGateway {
         this.wallet = new WalletTransactionGateway(http);
         this.ecp = new EcpAchTransactionGateway(http);
         this.sepadd = new SepaDdTransactionGateway(http);
-        this.batch = new BatchTransactionGateway(http);
         this.paypal = new PayPalTransactionGateway(http);
     }
 
--- src/lib/transaction/batch/BatchTransactionGateway.ts
+++
@@ -1,9 +0,0 @@
-import HttpClient from '../../http/HttpClient';
-
-export default class BatchTransactionGateway {
-    private http: HttpClient;
-
-    public constructor(http: HttpClient) {
-        this.http = http;
-    }
-}
