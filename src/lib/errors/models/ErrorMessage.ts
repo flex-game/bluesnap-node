78a54f22b8675dcc8cbe2fe587f89448150fa257
smrubin <sam@storyblocks.com>
2019-06-05 14:00:25 -0400

    Update ErrorMessage.ts

--- src/lib/errors/models/ErrorMessage.ts
+++ src/lib/errors/models/ErrorMessage.ts
@@ -1,13 +1,13 @@
-import AltTransactionErrors from '../enum/AltTransactionErrors';
-import BatchTransactionErrors from '../enum/BatchTransactionErrors';
-import CardTransactionErrors from '../enum/CardTransactionErrors';
-import FraudErrors from '../enum/FraudErrors';
-import HostedPaymentFieldErrors from '../enum/HostedPaymentFieldErrors';
-import MarketplaceErrors from '../enum/MarketplaceErrors';
-import RefundErrors from '../enum/RefundErrors';
-import SubscriptionAndBillingPlanErrors from '../enum/SubscriptionAndBillingPlanErrors';
-import VaultedShopperErrors from '../enum/VaultedShopperErrors';
-import WalletErrors from '../enum/WalletErrors';
+import {AltTransactionErrors} from "../enum/AltTransactionErrors";
+import {FraudErrors} from '../enum/FraudErrors';
+import {WalletErrors} from '../enum/WalletErrors';
+import {BatchTransactionErrors} from '../enum/BatchTransactionErrors';
+import {MarketplaceErrors} from '../enum/MarketplaceErrors';
+import {RefundErrors} from '../enum/RefundErrors';
+import {VaultedShopperErrors} from '../enum/VaultedShopperErrors';
+import {HostedPaymentFieldErrors} from '../enum/HostedPaymentFieldErrors';
+import {SubscriptionAndBillingPlanErrors} from '../enum/SubscriptionAndBillingPlanErrors';
+import {CardTransactionErrors} from '../enum/CardTransactionErrors';
 
 export interface ErrorMessage {
     errorName: AltTransactionErrors | BatchTransactionErrors | CardTransactionErrors | FraudErrors | HostedPaymentFieldErrors | MarketplaceErrors | RefundErrors | SubscriptionAndBillingPlanErrors | VaultedShopperErrors | WalletErrors;
